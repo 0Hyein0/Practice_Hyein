d9f8f0bc3cbac0a89db361105c5c6c7c7cdcf04a
Heyin Choi <97toki@naver.com>
2024-04-02 22:33:22 +0900

    백준 :: 2562

--- Practice_Hyein/src/Main.java
+++ Practice_Hyein/src/Main.java
@@ -5,25 +5,25 @@ public class Main
 	public static void main(String[] args)
 	{
 		Scanner sc = new Scanner(System.in);
+	
+		int[] array = new int[9];
+		int max = array[0];
+		int count = 0;
 		
-		int a = sc.nextInt();
-		int[] array = new int[a];
-
-		for(int i=0; i<a; i++)
+		for(int i = 0; i<9; i++)
+		{
 			array[i] = sc.nextInt();
 		
-		int max = array[0];
-		for(int i=0; i<array.length; i++)
 			if(array[i] > max)
+			{
 				max = array[i];
-		
-		int min = array[0];
-		for(int i=0; i<array.length; i++)
-			if(array[i] < min)
-				min = array[i];
-		
-		System.out.println(min + " " + max);
+				count = i+1;
+			}
+		}
+		System.out.println(max);
+		System.out.println(count);
 		
 		sc.close();
+	
 	}
 }
