8958f451bba3d0dd8efd091e73fb07bf56511697
Heyin Choi <97toki@naver.com>
2024-04-04 21:41:15 +0900

    백준 :: 3052

--- Practice_Hyein/src/Main.java
+++ Practice_Hyein/src/Main.java
@@ -5,18 +5,32 @@ public class Main
 	public static void main(String[] args)
 	{
 		Scanner sc = new Scanner(System.in);
-		int[] array = new int[30];
 		
-		for(int i=0; i<28; i++)
+		int[] array = new int[10];
+		int count = 0;
+		boolean flag;
+		
+		// 배열에 입력 받은 수를 42로 나눈 나머지 저장
+		for(int i=0; i<10; i++)
+			array[i] = sc.nextInt() % 42;
+		
+		
+		for(int i = 0; i<array.length; i++)
 		{
-			int n = sc.nextInt(); 
-			array[n-1] = 1;
+			flag = false;
+			
+			for(int j=i+1; j<array.length; j++)
+			{
+				if(array[i] == array[j])
+				{
+					flag = true;
+					break;
+				}
+			}
+			if(flag == false)
+				count++;
 		}
-
-		for(int i=0; i<30; i++)
-			if (array[i] != 1)
-				System.out.println(i+1);
-		
+		System.out.println(count);
 		sc.close();
 	}
 }
