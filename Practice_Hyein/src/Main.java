023c8f151a7ed9db2af66d9c69a115aafb4cf164
Heyin Choi <97toki@naver.com>
2024-04-07 21:29:28 +0900

    백준 :: 11654

--- Practice_Hyein/src/Main.java
+++ Practice_Hyein/src/Main.java
@@ -1,22 +1,19 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class Main
 {
-	public static void main(String[] args) throws IOException
+	public static void main(String[] args)
 	{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		Scanner sc = new Scanner(System.in);
 		
-		int n = Integer.parseInt(br.readLine());
+		int n = sc.nextInt();
+		String num = sc.next();
+		int result = 0;
 		
-		String[] word = new String[n];
+		for(int i=0; i<n; i++)
+			result += num.charAt(i)-'0';
 		
-		for (int i=0; i<n; i++)
-			word[i] = br.readLine();
-		
-		for (int i=0; i<n; i++)
-			System.out.println(word[i].substring(0,1) + "" + word[i].substring(word[i].length()-1, word[i].length()));
-	
+		System.out.println(result);
+		sc.close();
 	}
 }
