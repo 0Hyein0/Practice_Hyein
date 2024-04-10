bfbbe22b8a6dd8a49bd0d88becd437eb61a5b9b4
Heyin Choi <97toki@naver.com>
2024-04-10 23:00:19 +0900

    백준 :: 2675

--- Practice_Hyein/src/Main.java
+++ Practice_Hyein/src/Main.java
@@ -4,25 +4,25 @@ import java.io.InputStreamReader;
 
 public class Main
 {
-	public static void main(String[] args) throws IOException
+	public static void main(String[] args) throws NumberFormatException, IOException
 	{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	
+		int t = Integer.parseInt(br.readLine());
 		
-		String word = br.readLine();
-		int[] array = new int[26];		
-		
-		for(int i=0; i<26; i++)
-			array[i] = -1;				
-		
-		for(int i=0; i<word.length(); i++)
+		for (int i=0; i<t; i++)
 		{
-			char a = word.charAt(i);
-		
-			if(array[a - 'a'] == -1)	
-				array[a - 'a'] = i;		
+			String[] array = br.readLine().split(" ");		// 공백을 기준으로 나눔
+			int s = Integer.parseInt(array[0]);				// 1번 째 (0번 방)에는 각 문자를 반복할 횟수
+			String word = array[1];							// 2번 째 (1번 방)에는 문자열
+			
+			for (int j=0; j<word.length(); j++)
+				for(int k=0; k<s; k++)
+					System.out.print(word.charAt(j));
+			
+			System.out.println();
 		}
 		
-		for (int i=0; i < 26; i++)
-			System.out.print(array[i] + " ");
+		
 	}
 }
