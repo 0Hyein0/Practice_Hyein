e6fef23e8284401e480fc175f48138de9f395a22
Heyin Choi <97toki@naver.com>
2024-04-11 13:05:23 +0900

    백준 :: 1152

--- Practice_Hyein/src/Main.java
+++ Practice_Hyein/src/Main.java
@@ -1,28 +1,16 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main
 {
-	public static void main(String[] args) throws NumberFormatException, IOException
+	public static void main(String[] args) throws IOException
 	{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	
-		int t = Integer.parseInt(br.readLine());
+		String str = br.readLine();
+		StringTokenizer st = new StringTokenizer(str, " ");		// str을 공백을 기준으로 문자열로 나눔
 		
-		for (int i=0; i<t; i++)
-		{
-			String[] array = br.readLine().split(" ");		// 공백을 기준으로 나눔
-			int s = Integer.parseInt(array[0]);				// 1번 째 (0번 방)에는 각 문자를 반복할 횟수
-			String word = array[1];							// 2번 째 (1번 방)에는 문자열
-			
-			for (int j=0; j<word.length(); j++)
-				for(int k=0; k<s; k++)
-					System.out.print(word.charAt(j));
-			
-			System.out.println();
-		}
-		
-		
+		System.out.println(st.countTokens());		// st에 남아있는 토큰 갯수 반환
 	}
 }
