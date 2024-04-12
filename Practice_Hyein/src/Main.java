4bd9676b45f3a4c53ff9ccdfc2cd35cc77779c15
Heyin Choi <97toki@naver.com>
2024-04-13 01:52:42 +0900

    백준 :: 11718

--- Practice_Hyein/src/Main.java
+++ Practice_Hyein/src/Main.java
@@ -1,19 +1,17 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main
 {
 	public static void main(String[] args) throws IOException 
 	{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		String word;
 		
-		int a = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());		
-		int b = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
-		
-		System.out.println((a>b)?a:b);
-		
+		while((word = br.readLine()) != null)
+		{
+			System.out.println(word);
+		}
 	}
 }
