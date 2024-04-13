3a83f3e703a142f0f48bbdcf0fac1a477c5246fb
Heyin Choi <97toki@naver.com>
2024-04-13 20:49:23 +0900

    백준 ::

--- Practice_Hyein/src/Main.java
+++ Practice_Hyein/src/Main.java
@@ -7,11 +7,11 @@ public class Main
 	public static void main(String[] args) throws IOException 
 	{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String word;
+		String str;
 		
-		while((word = br.readLine()) != null)
+		while((str = br.readLine()) != null)
 		{
-			System.out.println(word);
+			System.out.println(str);
 		}
 	}
 }
