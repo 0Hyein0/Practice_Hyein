4ae132c482617a8ac021a40c9bce67cf9a3efed1
0Hyein0 <148781169+0Hyein0@users.noreply.github.com>
2024-04-14 02:22:48 +0900

    Update and rename Main.java to 11718번 :: 그대로 출력하기.java

--- Practice_Hyein/백준/11718번 :: 그대로 출력하기.java
+++ Practice_Hyein/백준/11718번 :: 그대로 출력하기.java
@@ -1,11 +1,13 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class Main
 {
 	public static void main(String[] args) throws IOException 
 	{
+		// BufferedReader
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String str;
 		
@@ -13,5 +15,15 @@ public class Main
 		{
 			System.out.println(str);
 		}
+
+		// Scanner
+		Scanner sc = new Scanner(System.in);
+		
+		while(sc.hasNextLine())
+		{
+			String str = sc.nextLine();
+			System.out.println(str);
+		}
+		sc.close();
 	}
 }
