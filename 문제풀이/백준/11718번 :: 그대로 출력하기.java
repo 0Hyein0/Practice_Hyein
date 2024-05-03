373c7bb7a5ec5a0fb7509d06fa487654375ac44c
0Hyein0 <148781169+0Hyein0@users.noreply.github.com>
2024-05-03 22:29:07 +0900

    11718번 :: 그대로 출력하기.java

--- 문제풀이/백준/11718번 :: 그대로 출력하기.java
+++ 문제풀이/백준/11718번 :: 그대로 출력하기.java
@@ -1,13 +1,11 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Scanner;
 
 public class Main
 {
 	public static void main(String[] args) throws IOException 
 	{
-		// BufferedReader
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String str;
 		
@@ -15,15 +13,5 @@ public class Main
 		{
 			System.out.println(str);
 		}
-
-		// Scanner
-		Scanner sc = new Scanner(System.in);
-		
-		while(sc.hasNextLine())
-		{
-			String str = sc.nextLine();
-			System.out.println(str);
-		}
-		sc.close();
 	}
 }
