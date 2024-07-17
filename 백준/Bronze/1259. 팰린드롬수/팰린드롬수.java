41d24f1eb1bd45dafd114eb78502f13439beb610
0Hyein0 <148781169+0Hyein0@users.noreply.github.com>
2024-07-18 01:14:28 +0900

    [Bronze I] Title: 팰린드롬수, Time: 128 ms, Memory: 13996 KB -BaekjoonHub

---
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        while (true) {
+            String str = br.readLine();
+
+            if(str.equals("0"))
+                break;
+            else {
+                String reverseStr = new StringBuilder(str).reverse().toString();
+                if(str.equals(reverseStr))
+                    sb.append("yes").append("\n");
+                else
+                    sb.append("no").append("\n");
+            }
+        }
+        System.out.println(sb);
+    }
+}
