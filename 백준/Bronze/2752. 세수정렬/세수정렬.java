4f9c37d0c797ea4cea4ef51066e3cbcd3263cdf3
0Hyein0 <148781169+0Hyein0@users.noreply.github.com>
2024-07-25 18:36:29 +0900

    [Bronze IV] Title: 세수정렬, Time: 112 ms, Memory: 14236 KB -BaekjoonHub

---
+++ 백준/Bronze/2752. 세수정렬/세수정렬.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        List<Integer> list = new ArrayList<>();
+
+        while(st.hasMoreTokens()) {
+            list.add(Integer.parseInt(st.nextToken()));
+        }
+
+        Collections.sort(list);
+
+        for(int num:list)
+            sb.append(num).append(" ");
+
+        System.out.println(sb);
+
+    }
+}
